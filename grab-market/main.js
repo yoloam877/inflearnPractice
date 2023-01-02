c58a610c0f7d4ac7cba3697c9e0f722eaa663545
yoloam877 <khw734100@gmail.com>
2023-01-02 21:09:08 +0900

    0102 javascript axios | to section 4

--- grab-market/main.js
+++ grab-market/main.js
@@ -4,53 +4,67 @@
 
 // =====================================================================
 
+// axios 네트워크 통신
+axios
+  .get("url주소") //  postman 다시 해볼 것
+  .then((result) => {
+    console.log(result);
+
+    const products = result.data.products; //  결과의 data의 products에 접근
+
+    let productHtml = ""; //  빈 문자열 선언
+
+    for (let i = 0; i < products.length; i++) {
+      let product = products[i]; //  해당 생성한 배열 인덱스에 따른 정보에 대한 product 변수 생성
+
+      productHtml += `<div class="product-card">
+        <div>
+          <img
+            class="product-img"
+            src="${product.imageUrl}"
+            alt=""
+          />
+        </div>
+
+        <div class="product-contents">
+          <span class="product-name">${product.name}</span>
+          <span class="product-price">${product.price}</span>
+          <div class="product-seller">
+            <img
+              class="product-avatar"
+              src="images/icons/avatar.png"
+              alt=""
+            />
+            <span>${product.seller}</span>
+          </div>
+        </div>
+        </div>`;
+    }
+
+    document.querySelector("#product-list").innerHTML = productHtml;
+  })
+  .cathe((err) => {
+    console.log(err);
+  });
+
 // 해당 상품에 대한 배열 생성
-let products = [
-  {
-    name: "농구공",
-    price: 10000,
-    seller: "조던",
-    imageUrl: "images/products/basketball1.jpeg",
-  },
-  {
-    name: "축구공",
-    price: 50000,
-    seller: "메시",
-    imageUrl: "images/products/soccerball1.jpg",
-  },
-  {
-    name: "키보드",
-    price: 10000,
-    seller: "그랩",
-    imageUrl: "images/products/keyboard1.jpg",
-  },
-];
-
-let productHtml = ""; //  빈 문자열 선언
-
-for (let i = 0; i < products.length; i++) {
-  let product = products[i]; //  해당 생성한 배열 인덱스에 따른 정보에 대한 product 변수 생성
-
-  productHtml += `<div class="product-card">
-    <div>
-      <img
-        class="product-img"
-        src="${product.imageUrl}"
-        alt=""
-      />
-    </div>
-    
-    <div class="product-contents">
-      <span class="product-name">${product.name}</span>
-      <span class="product-price">${product.price}</span>
-      <div class="product-seller">
-        <img
-          class="product-avatar"
-          src="images/icons/avatar.png"
-          alt=""
-        />
-        <span>${product.seller}</span>
-      </div>
-    </div>
-    </div>`;
-}
+// let products = [
+//   {
+//     name: "농구공",
+//     price: 10000,
+//     seller: "조던",
+//     imageUrl: "images/products/basketball1.jpeg",
+//   },
+//   {
+//     name: "축구공",
+//     price: 50000,
+//     seller: "메시",
+//     imageUrl: "images/products/soccerball1.jpg",
+//   },
+//   {
+//     name: "키보드",
+//     price: 10000,
+//     seller: "그랩",
+//     imageUrl: "images/products/keyboard1.jpg",
+//   },
+// ];
